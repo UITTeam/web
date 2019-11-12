610d7956e0fe38d52cdb21725f11c5415a74850b
Mai Như Ngọc <mnngoc@users.noreply.github.com>
2019-11-12 10:42:22 +0700

    update home

---
+++ js/home.js
@@ -0,0 +1,49 @@
+
+$(document).ready(function(){
+	//fancybox.js init
+	$('.fancybox').fancybox({
+		openEffect : 'none',
+		closeEffect : 'none',
+		prevEffect : 'none',
+		nextEffect : 'none',
+
+		arrows : false,
+		helpers : {
+			media : {},
+			buttons : {}
+		}
+	});
+
+	wow = new WOW(
+	    {
+		  animateClass: 'animated',
+		  mobile: false,
+		  offset: 100
+		}
+	);
+	wow.init();
+
+});
+
+
+$(function(){
+	$('#btnSubmit').on('click', function(e)
+	{
+		var username = $('#username').val(),
+			pass = $('#password').val(),
+			correct_user= 'admin',
+			correct_pass= 'admin';
+
+		 if (username === correct_user && pass === correct_pass)
+		  {
+			  e.preventDefault();				  	 
+			  window.location.href = './user-info.html';
+		  }
+	  else
+		  {
+		  	e.preventDefault();
+	 	 	swal("Error!", "Invalid password or username", "error").delay(1);
+		  }
+	});
+
+})
