af96606a7bf58f9954190edfec46dfceae0a3ce7
Mai Như Ngọc <mnngoc@users.noreply.github.com>
2019-11-13 09:47:16 +0700

    update login

--- js/home.js
+++ js/home.js
@@ -26,6 +26,39 @@ $(document).ready(function(){
 });
 
 $(function(){
+
+	$('#btnSubmit').on('click', function(e)
+	{
+		var username = $('#username').val(),
+			pass = $('#password').val(),
+			correct_user= 'admin',
+			correct_pass= 'admin';
+		 if (username === correct_user && pass === correct_pass)
+		  {
+			 				  	 
+			 $.ajax({
+				type : 'POST', //Sử dụng kiểu gửi dữ liệu POST
+				url : 'php/login.php', //gửi dữ liệu sang trang data.php
+				data : username, //dữ liệu sẽ được gửi
+				success : function(data)  // Hàm thực thi khi nhận dữ liệu được từ server
+						  { 
+							 if(data == 'false') 
+							 {
+							   alert('Không có người dùng');
+							 }else{
+							   $('#btnLogin').append('<p>hihi<p>');
+							 }
+						  }
+				});
+		  }
+	  else
+		  {
+		  	e.preventDefault();
+	 	 	swal("Error!", "Invalid password or username ủa ủa", "error");
+		  }
+		 
+	});
+/*
 	$('#btnSubmit').on('click', function(e)
 	{
 		var username = $('#username').val(),
@@ -43,5 +76,6 @@ $(function(){
 	 	 	swal("Error!", "Invalid password or username", "error");
 		  }
 	});
+	*/
 
 })
